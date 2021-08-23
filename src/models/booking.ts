891c2ba49a5b35a743980d7845451f270f321ce9
MattSedmak <sedmakmatt@gmail.com>
2021-08-23 10:42:58 +0200

    Initial commit - pair programming

---
+++ src/models/booking.ts
@@ -0,0 +1,43 @@
+import { IBooking } from '../types/IBooking';
+import { model, Schema } from 'mongoose';
+
+const bookingSchema: Schema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+    },
+    guests: {
+      type: Number,
+      required: true,
+    },
+    seating: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<IBooking>('Booking', bookingSchema);
---
+++ src/types/IBooking.ts
@@ -0,0 +1,12 @@
+import { Document } from 'mongoose';
+
+export interface IBooking extends Document {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobile: number;
+  guests: number;
+  seating: number;
+  date: Date;
+}
