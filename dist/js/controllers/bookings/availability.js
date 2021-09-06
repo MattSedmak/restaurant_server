d855d1dcca7da3ec226e346d24b3dbcb5bcb4e4f
johced <johan.cederstrand@bahnhof.se>
2021-09-06 17:34:53 +0200

    Added logic for the tables 14 tables can now book 1 table and send back false if requesting 2 tables

--- dist/js/controllers/bookings/availability.js
+++ dist/js/controllers/bookings/availability.js
@@ -27,20 +27,15 @@ const getAvailability = (req, res) => __awaiter(void 0, void 0, void 0, function
             let guests = seatingTimeResults[i].guests;
             let oneDate = seatingTimeResults[i].date;
             guests > 6 ? (numberOfTables = 2) : (numberOfTables = 1);
-            // Ta reda på om ListofDate har ett objekt som har samma date som på rad 28.
+            guestNumber > 6 ? (requestedTables = 2) : (requestedTables = 1);
             const found = listOfDates.find((d) => d.date.toLocaleString().substring(0, 10) ===
                 oneDate.toLocaleString().substring(0, 10));
-            guestNumber > 6 ? (requestedTables = 2) : (requestedTables = 1);
-            // Om JA, ta objekt i listOfDate och lägg på nrOfTables till IAvailable tables.
             if (found) {
-                found.tables += numberOfTables;
-                // 15 är max bord
-                found.tables + requestedTables >= 16
-                    ? (found.isAvailable = false)
-                    : (found.isAvailable = true);
+                let tableCheck = (found.tables += numberOfTables);
+                let test = tableCheck + requestedTables;
+                test >= 16 ? (found.isAvailable = false) : (found.isAvailable = true);
             }
             else {
-                // OM NEJ, skapa ett objekt
                 listOfDates.push({
                     date: oneDate,
                     tables: numberOfTables,
@@ -49,6 +44,7 @@ const getAvailability = (req, res) => __awaiter(void 0, void 0, void 0, function
             }
         }
         res.status(200).json(listOfDates);
+        console.log(listOfDates);
     }
     catch (error) {
         throw error;
