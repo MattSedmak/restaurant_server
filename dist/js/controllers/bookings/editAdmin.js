76c81a54ece9e67bca16e2a35a1148a8a54982aa
johced <johan.cederstrand@bahnhof.se>
2021-09-07 16:29:49 +0200

    fixed the logic for handleing edit on existing booking date

--- dist/js/controllers/bookings/editAdmin.js
+++ dist/js/controllers/bookings/editAdmin.js
@@ -20,7 +20,6 @@ const getEditAdmin = (req, res) => __awaiter(void 0, void 0, void 0, function* (
         let guestNumber = Number(req.query.guests);
         let guestDate = String(req.query.date);
         let id = String(req.query.id);
-        console.log('query id: ' + id);
         const selectedSeating = yield booking_1.default.find();
         let selectedDateResults = selectedSeating.filter((selected) => selected.date.toLocaleString('sv-SE').substring(0, 10) ===
             guestDate.substring(0, 10));
@@ -28,42 +27,35 @@ const getEditAdmin = (req, res) => __awaiter(void 0, void 0, void 0, function* (
         let numberOfTables = 1;
         let currentTables = 0;
         let newTables = 0;
-        // let testingObject: {} = {};
         let isAvailable = {
             available: true,
         };
+        let totalTables = 0;
         for (let i = 0; i < timeResults.length; i++) {
             let guests = timeResults[i].guests;
             guests > 6 ? (numberOfTables = 2) : (numberOfTables = 1);
             guestNumber > 6 ? (newTables = 2) : (newTables = 1);
-            let totalTables = (currentTables += numberOfTables);
-            let totalTablesSameDateEdit = totalTables - newTables;
-            // totalTablesSameDateEdit + newTables >= 16
-            if (timeResults[i].id.toString() === id &&
-                totalTablesSameDateEdit + newTables >= 16) {
-                // totalTablesSameDateEdit + newTables >= 16
-                //   ? (isAvailable.available = false)
-                //   : (isAvailable.available = true);
-                // console.log(timeResults[i].id.toString());
-                // console.log(timeResults[i]);
-                isAvailable.available = false;
-                console.log('första if körs');
-            }
-            else if (totalTables + newTables >= 16) {
-                isAvailable.available = false;
-                console.log('andra if id: ' + timeResults[i].id.toString());
-                console.log('query id: ' + id);
-                console.log('andra if körs');
-                // NÄR EN BOKNING PÅ SAMMA DAG ÄNDRAS TILL 2 från 1 bildas nytt id?!?!
-                // console.log(timeResults[i].id.toString());
-                //console.log(timeResults[i]);
-                //console.log(id);
-            }
-            // if (totalTables + newTables >= 16) {
-            //   isAvailable.available = false;
-            // }
+            totalTables = currentTables += numberOfTables;
         }
-        console.log(isAvailable);
+        let foundOwnBooking = false;
+        let tablesFromCurrentBooking = 0;
+        for (let i = 0; i < timeResults.length; i++) {
+            if (timeResults[i].id.toString() === id) {
+                foundOwnBooking = true;
+                tablesFromCurrentBooking = Math.ceil(timeResults[i].guests / 6);
+            }
+        }
+        if (foundOwnBooking) {
+            if (totalTables - tablesFromCurrentBooking + newTables >= 16) {
+                isAvailable.available = false;
+                foundOwnBooking = true;
+            }
+        }
+        else {
+            if (totalTables + newTables >= 16) {
+                isAvailable.available = false;
+            }
+        }
         res.status(200).json(isAvailable);
     }
     catch (error) {
--- src/controllers/bookings/editAdmin.ts
+++ src/controllers/bookings/editAdmin.ts
@@ -1,8 +1,6 @@
 import { Response, Request } from 'express';
 import { IBooking } from '../../types/IBooking';
 import Booking from '../../models/booking';
-import { isatty } from 'tty';
-import { AnyARecord } from 'dns';
 
 const getEditAdmin = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -10,7 +8,7 @@ const getEditAdmin = async (req: Request, res: Response): Promise<void> => {
     let guestNumber: number = Number(req.query.guests);
     let guestDate: string = String(req.query.date);
     let id: string = String(req.query.id);
-    console.log('query id: ' + id);
+
     const selectedSeating: IBooking[] = await Booking.find();
 
     let selectedDateResults = selectedSeating.filter(
@@ -26,48 +24,42 @@ const getEditAdmin = async (req: Request, res: Response): Promise<void> => {
     let numberOfTables: number = 1;
     let currentTables: number = 0;
     let newTables: number = 0;
-    // let testingObject: {} = {};
+
     let isAvailable = {
       available: true,
     };
+
+    let totalTables: number = 0;
 
     for (let i = 0; i < timeResults.length; i++) {
       let guests = timeResults[i].guests;
       guests > 6 ? (numberOfTables = 2) : (numberOfTables = 1);
       guestNumber > 6 ? (newTables = 2) : (newTables = 1);
 
-      let totalTables = (currentTables += numberOfTables);
-      let totalTablesSameDateEdit = totalTables - newTables;
-      // totalTablesSameDateEdit + newTables >= 16
-
-      if (
-        timeResults[i].id.toString() === id &&
-        totalTablesSameDateEdit + newTables >= 16
-      ) {
-        // totalTablesSameDateEdit + newTables >= 16
-        //   ? (isAvailable.available = false)
-        //   : (isAvailable.available = true);
-        // console.log(timeResults[i].id.toString());
-        // console.log(timeResults[i]);
-        isAvailable.available = false;
-        console.log('första if körs');
-      } else if (totalTables + newTables >= 16) {
-        isAvailable.available = false;
-        console.log('andra if id: ' + timeResults[i].id.toString());
-        console.log('query id: ' + id);
-        console.log('andra if körs');
-        // NÄR EN BOKNING PÅ SAMMA DAG ÄNDRAS TILL 2 från 1 bildas nytt id?!?!
-        // console.log(timeResults[i].id.toString());
-        //console.log(timeResults[i]);
-        //console.log(id);
-      }
-
-      // if (totalTables + newTables >= 16) {
-      //   isAvailable.available = false;
-      // }
+      totalTables = currentTables += numberOfTables;
     }
 
-    console.log(isAvailable);
+    let foundOwnBooking = false;
+    let tablesFromCurrentBooking: number = 0;
+
+    for (let i = 0; i < timeResults.length; i++) {
+      if (timeResults[i].id.toString() === id) {
+        foundOwnBooking = true;
+        tablesFromCurrentBooking = Math.ceil(timeResults[i].guests / 6);
+      }
+    }
+
+    if (foundOwnBooking) {
+      if (totalTables - tablesFromCurrentBooking + newTables >= 16) {
+        isAvailable.available = false;
+        foundOwnBooking = true;
+      }
+    } else {
+      if (totalTables + newTables >= 16) {
+        isAvailable.available = false;
+      }
+    }
+
     res.status(200).json(isAvailable);
   } catch (error) {
     throw error;
