9700eeb5ce39e5658727929c9d6a5ff7a09890ca
johced <johan.cederstrand@bahnhof.se>
2021-09-07 10:05:24 +0200

    still trying to fix logic for already owning a table and updating to 2 tables on same date

--- dist/js/controllers/bookings/editAdmin.js
+++ dist/js/controllers/bookings/editAdmin.js
@@ -19,28 +19,51 @@ const getEditAdmin = (req, res) => __awaiter(void 0, void 0, void 0, function* (
         let seatingTime = Number(req.query.seating);
         let guestNumber = Number(req.query.guests);
         let guestDate = String(req.query.date);
+        let id = String(req.query.id);
+        console.log('query id: ' + id);
         const selectedSeating = yield booking_1.default.find();
         let selectedDateResults = selectedSeating.filter((selected) => selected.date.toLocaleString('sv-SE').substring(0, 10) ===
             guestDate.substring(0, 10));
         let timeResults = selectedDateResults.filter((selected) => seatingTime === selected.seating);
         let numberOfTables = 1;
-        let totalTables = 0;
-        let currentGuests = 0;
+        let currentTables = 0;
+        let newTables = 0;
+        // let testingObject: {} = {};
         let isAvailable = {
             available: true,
         };
         for (let i = 0; i < timeResults.length; i++) {
             let guests = timeResults[i].guests;
             guests > 6 ? (numberOfTables = 2) : (numberOfTables = 1);
-            guestNumber > 6 ? (currentGuests = 2) : (currentGuests = 1);
-            totalTables += numberOfTables;
-            console.log('isavailable ', isAvailable);
-            if (totalTables + currentGuests >= 15) {
-                console.log(isAvailable);
+            guestNumber > 6 ? (newTables = 2) : (newTables = 1);
+            let totalTables = (currentTables += numberOfTables);
+            let totalTablesSameDateEdit = totalTables - newTables;
+            // totalTablesSameDateEdit + newTables >= 16
+            if (timeResults[i].id.toString() === id &&
+                totalTablesSameDateEdit + newTables >= 16) {
+                // totalTablesSameDateEdit + newTables >= 16
+                //   ? (isAvailable.available = false)
+                //   : (isAvailable.available = true);
+                // console.log(timeResults[i].id.toString());
+                // console.log(timeResults[i]);
                 isAvailable.available = false;
+                console.log('första if körs');
             }
+            else if (totalTables + newTables >= 16) {
+                isAvailable.available = false;
+                console.log('andra if id: ' + timeResults[i].id.toString());
+                console.log('query id: ' + id);
+                console.log('andra if körs');
+                // NÄR EN BOKNING PÅ SAMMA DAG ÄNDRAS TILL 2 från 1 bildas nytt id?!?!
+                // console.log(timeResults[i].id.toString());
+                //console.log(timeResults[i]);
+                //console.log(id);
+            }
+            // if (totalTables + newTables >= 16) {
+            //   isAvailable.available = false;
+            // }
         }
-        // console.log(isAvailable);
+        console.log(isAvailable);
         res.status(200).json(isAvailable);
     }
     catch (error) {
--- src/controllers/bookings/editAdmin.ts
+++ src/controllers/bookings/editAdmin.ts
@@ -1,12 +1,16 @@
 import { Response, Request } from 'express';
 import { IBooking } from '../../types/IBooking';
 import Booking from '../../models/booking';
+import { isatty } from 'tty';
+import { AnyARecord } from 'dns';
 
 const getEditAdmin = async (req: Request, res: Response): Promise<void> => {
   try {
     let seatingTime: number = Number(req.query.seating);
     let guestNumber: number = Number(req.query.guests);
     let guestDate: string = String(req.query.date);
+    let id: string = String(req.query.id);
+    console.log('query id: ' + id);
     const selectedSeating: IBooking[] = await Booking.find();
 
     let selectedDateResults = selectedSeating.filter(
@@ -20,8 +24,9 @@ const getEditAdmin = async (req: Request, res: Response): Promise<void> => {
     );
 
     let numberOfTables: number = 1;
-    let totalTables: number = 0;
-    let currentGuests: number = 0;
+    let currentTables: number = 0;
+    let newTables: number = 0;
+    // let testingObject: {} = {};
     let isAvailable = {
       available: true,
     };
@@ -29,16 +34,40 @@ const getEditAdmin = async (req: Request, res: Response): Promise<void> => {
     for (let i = 0; i < timeResults.length; i++) {
       let guests = timeResults[i].guests;
       guests > 6 ? (numberOfTables = 2) : (numberOfTables = 1);
-      guestNumber > 6 ? (currentGuests = 2) : (currentGuests = 1);
+      guestNumber > 6 ? (newTables = 2) : (newTables = 1);
 
-      totalTables += numberOfTables;
-      console.log('isavailable ', isAvailable);
-      if (totalTables + currentGuests >= 15) {
-        console.log(isAvailable);
+      let totalTables = (currentTables += numberOfTables);
+      let totalTablesSameDateEdit = totalTables - newTables;
+      // totalTablesSameDateEdit + newTables >= 16
+
+      if (
+        timeResults[i].id.toString() === id &&
+        totalTablesSameDateEdit + newTables >= 16
+      ) {
+        // totalTablesSameDateEdit + newTables >= 16
+        //   ? (isAvailable.available = false)
+        //   : (isAvailable.available = true);
+        // console.log(timeResults[i].id.toString());
+        // console.log(timeResults[i]);
         isAvailable.available = false;
+        console.log('första if körs');
+      } else if (totalTables + newTables >= 16) {
+        isAvailable.available = false;
+        console.log('andra if id: ' + timeResults[i].id.toString());
+        console.log('query id: ' + id);
+        console.log('andra if körs');
+        // NÄR EN BOKNING PÅ SAMMA DAG ÄNDRAS TILL 2 från 1 bildas nytt id?!?!
+        // console.log(timeResults[i].id.toString());
+        //console.log(timeResults[i]);
+        //console.log(id);
       }
+
+      // if (totalTables + newTables >= 16) {
+      //   isAvailable.available = false;
+      // }
     }
-    // console.log(isAvailable);
+
+    console.log(isAvailable);
     res.status(200).json(isAvailable);
   } catch (error) {
     throw error;
